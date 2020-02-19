d86d65413acf6d63ea620a8d58fdef59a879b8a0
Gwirarien <obangbranes@yahoo.com>
2020-02-19 20:32:15 +0200

    Small app structure changes

---
+++ server/app/models.py
@@ -0,0 +1,23 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    data = db.relationship('Data', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}', '{self.image_file}')"
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    input = db.Column(db.Integer)
+    public_key = db.Column(db.Integer)
+    enc_data_matrix = db.Column(db.Float)
+    enc_solution_matrix = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"Post('{self.public_key}', '{self.enc_data_matrix}', '{self.enc_solution_matrix}')"
