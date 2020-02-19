9d9baa247971ff0ea677457477df9375811e2062
Gwirarien <obangbranes@yahoo.com>
2020-02-19 19:30:44 +0200

    Added support for database using SQLAlchemy

--- server/run.py
+++ server/run.py
@@ -1,9 +1,33 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    data = db.relationship('Data', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}', '{self.image_file}')"
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    input = db.Column(db.Integer)
+    public_key = db.Column(db.Integer)
+    enc_data_matrix = db.Column(db.Float)
+    enc_solution_matrix = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"Post('{self.public_key}', '{self.enc_data_matrix}', '{self.enc_solution_matrix}')"
 
 @app.route("/")
 @app.route("/home")
